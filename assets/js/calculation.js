3ccb47a7b2c0a597bfdd6bf46599323edcb8e69a
petch <tanee.kumpijit@gmail.com>
2019-02-06 12:41:36 +0700

    add calculation function (before test)

--- assets/js/calculation.js
+++ assets/js/calculation.js
@@ -22,11 +22,21 @@ function Party(name, partylistAppliedNum) {
   this.partylistAppliedNum = partylistAppliedNum;
   this.partylistWonNum = 0;
 
+  this.partyListByScore = 0;
+  this.sumScore = 0;
+
   this.applyForDistrict = function(id) {
     if(this.districtAppliedList[id]) return false; //already applied
     if(!listOfDistrict[id]) throw 'District not found'
     this.districtAppliedList[id] = {
       won: false,
+    }
+  }
+
+  this.countLocalWon = function() {
+    this.localWonNum = 0;
+    for(let districtId in this.districtAppliedList) {
+      if(this.districtAppliedList[districtId].won) this.localWonNum++;
     }
   }
 }
@@ -57,12 +67,76 @@ function District(name) {
     }
     let wonParty = listOfParty[winnerId];
     wonParty.districtAppliedList[this.id].won = true;
+    wonParty.countLocalWon();
     this.partyWon = winnerId;
   }
 }
 
 function triggerCalculate() {
-  //to be done
+  let numCandidate = localRepresentativeNum + partylistRepresentativeNum;
+  let sumScore = 0;
+  let partyToConsider = [];
+  let sumPartyListNeed = 0;
+  let partylistAssignedNum = 0;
+
+  let decimalHandle = [];
+
+  listOfDistrict.forEach((district) => {
+    //no vote
+    if(district.partyWon === 0) {
+      //this district's score is void
+      return;
+    }
+    for(let partyId in district.score) {
+      sumScore += district.score[partyId];
+    }
+  });
+
+  listOfParty.forEach((party) => {
+    let candidateByScore = (party.sumScore*numCandidate)/sumScore;
+    if(party.localWonNum >= candidateByScore) {
+      party.partyListByScore = 0;
+      party.partylistWonNum = 0;
+    } else {
+      partyToConsider.push(party);
+      party.partyListByScore = parseInt(Math.floor(candidateByScore - party.localWonNum));
+      sumPartyListNeed += party.partyListByScore;
+
+      decimalHandle.push({
+        party: party,
+        decimal: candidateByScore - party.localWonNum - this.partyListByScore,
+        avgScore: party.sumScore/(candidateByScore - party.localWonNum - this.partyListByScore),
+        random: Date.now()%1000,
+      });
+    }
+  });
+
+  if(sumPartyListNeed > 150) {
+    partyToConsider.forEach((party) => {
+      party.partyListByScore = parseInt(Math.floor(
+        party.partyListByScore*partylistRepresentativeNum/sumPartyListNeed)
+      );
+    });
+  }
+  partyToConsider.forEach((party) => {
+    party.partylistWonNum = party.partyListByScore;
+    partylistAssignedNum += party.partyListByScore;
+  });
+
+  decimalHandle.sort((a,b) => {
+    if(a.decimal !== b.decimal) return a.decimal - b.decimal;
+    if(a.avgScore !== b.avgScore) return a.avgScore - b.avgScore;
+    return a.random - b.random;
+  })
+
+  //handle decimal
+  let indexDecimal = 0;
+  while(partylistAssignedNum < 150) {
+    decimalHandle[indexDecimal].party.partylistWonNum++;
+    partylistAssignedNum++;
+    indexDecimal++;
+    indexDecimal %= decimalHandle.length;
+  }
 }
 
 //=================================================================================
@@ -75,7 +149,6 @@ function createNewParty(name, partylistAppliedNum) {
 
 function createNewDistrict(name) {
   let newDistrict = new District(name);
-  //apply no vote
   applyPartyAtDistrict(novote, newDistrict);
   return newDistrict;
 }
@@ -87,6 +160,10 @@ function applyPartyAtDistrict(party, district) {
 
 function setScore(party, district, score) {
   if(!district.score[party.id]) throw 'Party did not apply at this district';
+  //old score
+  party.sumScore -= district.score[party.id];
+  //new score
+  party.sumScore += score; 
   district.score[party.id] = score;
   district.triggerWon();
   triggerCalculate();
